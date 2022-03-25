4a0b778aa4763f32ba3c9fdb7a51cb42083eebb8
Carlos Arturo Vargas <carlosospino13@gmail.com>
2022-03-25 13:52:57 -0500

    Correción espacio vacío al final de cada IP.

--- helpers/crearArchivo.js
+++ helpers/crearArchivo.js
@@ -8,7 +8,7 @@ const crearArchivoIPsOrdenadas = (IPsOrdenadas) => {
         let salida = "";
 
         IPsOrdenadas.forEach((ip, index, arr) => {
-            salida += `${ip.octeto1}.${ip.octeto2}.${ip.octeto3}.${ip.octeto4}/${ip.mascara} ${index !== arr.length - 1 ? '\n' : ''}`;
+            salida += `${ip.octeto1}.${ip.octeto2}.${ip.octeto3}.${ip.octeto4}/${ip.mascara}${index !== arr.length - 1 ? '\n' : ''}`;
         });
 
         fs.writeFileSync(rutaNuevoArchivo, salida);
